a87bd2530261abfd1391ede7cff6d154e195aba8
I527245 <rajeev.kumar13@sap.com>
2024-03-25 21:46:58 +0530

    to do task

--- practice/src/component/ToDo.js
+++ practice/src/component/ToDo.js
@@ -1,19 +1,57 @@
 import React, { useRef, useState } from "react";
+import { MdEdit } from "react-icons/md";
+import { MdDelete } from "react-icons/md";
 const ToDo = () => {
-  const [toDo, setToDo] = useState([]);
+  const [toDos, setToDos] = useState([]);
   const ref = useRef();
 
-  const handleAddButtonTask = () => {
-    console.log(ref.current .value);
-    setToDo(...toDo, ref.current.value)
-  }
-  console.log(toDo);
+  const handleAddTask  = () => {
+    setToDos((toDos) => [
+      ...toDos,
+      { task: ref.current.value, key: toDos.length + 1 },
+    ]);
+  };
+
+  const handleEdit = (key) => {
+    console.log(key);
+  };
+
+  const handleDelete = (key2) => {
+    let res = toDos.filter((task, key) => {
+      return key2 - 1 !== key;
+    });
+    setToDos(res);
+  };
 
   return (
     <div>
-      <h1>ToDo</h1>
-      <input ref = {ref} type="text" placeholder="Enter To Do Task" />
-      <button onClick={handleAddButtonTask}>Add</button>
+      <h1 className="toDoHeading">ToDo</h1>
+      <div className="toDoCreate">
+        <input ref={ref} type="text" placeholder="Enter To Do Task" />
+        <button onClick={handleAddTask}>Add</button>
+      </div>
+      <div className="toDoList">
+        <ul>
+          {toDos?.map(({ task, key }) => {
+            return task ? (
+              <div className="toDoItems">
+                <li>{task}</li>{" "}
+                <MdEdit
+                  onClick={() => {
+                    handleEdit(key);
+                  }}
+                  style={{ fontSize: "1.3em" }}
+                />
+                <MdDelete
+                  onClick={() => {
+                    handleDelete(key);
+                  }}
+                />
+              </div>
+            ) : null;
+          })}
+        </ul>
+      </div>
     </div>
   );
 };
