5761eac987d855a95dd7b37754912f1dca5afd84
I527245 <rajeev.kumar13@sap.com>
2024-06-13 09:50:00 +0530

    lazy load

--- practice/src/App.js
+++ practice/src/App.js
@@ -12,6 +12,7 @@ import WhackAMole from "./component/WhackAMole";
 import Debouncing from "./component/Debouncing";
 import Throttling from "./component/Throttling";
 import Calculator from "./component/Calculator";
+import LazyLoadDemo from "./component/LazyLoadDemo";
 
 function App() {
   const router = createBrowserRouter([
@@ -50,6 +51,10 @@ function App() {
     {
       path: "/calculator",
       element: <Calculator />,
+    },
+    {
+      path: "/lazyload",
+      element: <LazyLoadDemo />,
     },
   ]);
   return (
